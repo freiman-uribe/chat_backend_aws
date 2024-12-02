fbfa68bc87b8bd482054325222b4ab52af186f8f
FreimanRuiz <freiman@bewe.io>
2024-12-01 22:51:20 -0500

    add update

--- src/infrastructure/config/connectMongo.js
+++ src/infrastructure/config/connectMongo.js
@@ -5,9 +5,12 @@ const connect = async () => {
     console.log("process.env.URL_CONNECT>>>", process.env.URL_CONNECT);
     
     try {
-        await mongoose.connect("mongodb://localhost:27017/classRoom", {
-          useUnifiedTopology: true,
-        });
+        await mongoose.connect(
+          "mongodb://root:example@mongo:27017/classRoom?authSource=admin",
+          {
+            useUnifiedTopology: true,
+          }
+        );
         console.log('Conexion exitosa a la BD');
     } catch (error) {
         console.error('🚀 ~ connect ~ error:', error);

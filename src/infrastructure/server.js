ddc6f9433052a70c2e9bc1eed37c6ff2160aee82
FreimanRuiz <freiman@bewe.io>
2024-12-01 23:11:53 -0500

    add update

--- src/infrastructure/server.js
+++ src/infrastructure/server.js
@@ -23,6 +23,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/", (req, res) => res.status(404).send("Not found"));
 app.use("/api/chat", chatRoutes(io));
 
 io.on("connection", (socket) => {
